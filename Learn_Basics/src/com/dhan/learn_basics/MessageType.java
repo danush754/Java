4bbd69c6427475956697161aba8b7e34241b7409
Dhanush <danush2228s@gmail.com>
2023-10-23 11:30:24 +0530

    New folder has been added to practice basics

---
+++ Learn_Basics/src/com/dhan/learn_basics/MessageType.java
@@ -0,0 +1,19 @@
+package com.dhan.learn_basics;
+
+public enum MessageType {
+	A(Priority.HIGH),B(Priority.MEDIUM),C(Priority.LOW),D(Priority.LOW);
+	
+	private Priority priority;
+	
+	private MessageType(Priority priority) {
+		this.priority = priority ;
+	}
+	
+	public Priority getPriority() {
+		return this.priority;
+		
+	}
+
+	
+
+}
---
+++ Learn_Basics/src/com/dhan/learn_basics/Testclass.java
@@ -0,0 +1,10 @@
+package com.dhan.learn_basics;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test program ");
+
+	}
+
+}
