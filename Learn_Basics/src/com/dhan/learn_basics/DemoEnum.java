7e067cd79e9c90f72348ed90609d310f33163d4d
Dhanush <danush2228s@gmail.com>
2023-11-01 11:51:38 +0530

    Practiced Enum and few Interview questions

--- Learn_Basics/src/com/dhan/learn_basics/DemoEnum.java
+++ Learn_Basics/src/com/dhan/learn_basics/DemoEnum.java
@@ -2,32 +2,32 @@ package com.dhan.learn_basics;
 
 import java.util.Scanner;
 
-public class DemoEnum {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		while (true) {
-			System.out.println("Please, enter message type to check it priority: ");
-			String userInput = sc.next();
-
-			if (isValidMessageType(userInput)) {
-				MessageType messagetype = MessageType.valueOf(userInput.toUpperCase());
-				System.out.println(messagetype.getPriority());
-				
-				break;
-			} else {
-				System.out.println("Please, enter valid "
-						+ "message type. Only 'A', 'B', 'C' or 'D' are allowed");
-				continue;
-			}
-
-		}
-	}
-
-	private static boolean isValidMessageType(String userInput) {
-		return userInput.equalsIgnoreCase("a") || userInput.equalsIgnoreCase("b") || userInput.equalsIgnoreCase("c") || userInput.equalsIgnoreCase("d") ;
-	}
+class DemoEnum {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		while (true) {
+//			System.out.println("Please, enter message type to check it priority: ");
+//			String userInput = sc.next();
+//
+//			if (isValidMessageType(userInput)) {
+//				MessageType messagetype = MessageType.valueOf(userInput.toUpperCase());
+//				System.out.println(messagetype.getPriority());
+//				
+//				break;
+//			} else {
+//				System.out.println("Please, enter valid "
+//						+ "message type. Only 'A', 'B', 'C' or 'D' are allowed");
+//				continue;
+//			}
+//
+//		}
+//	}
+//
+//	private static boolean isValidMessageType(String userInput) {
+//		return userInput.equalsIgnoreCase("a") || userInput.equalsIgnoreCase("b") || userInput.equalsIgnoreCase("c") || userInput.equalsIgnoreCase("d") ;
+//	}
 	
-    
+    protected int x,y;
 	
 
 }
---
+++ Learn_Basics/src/com/dhan/learn_basics/Exam.java
@@ -0,0 +1,10 @@
+package com.dhan.learn_basics;
+
+public class Exam {
+
+	public static void main(String[] args) {
+		float a = 35/0;
+		System.out.println(a);
+	}
+
+}
