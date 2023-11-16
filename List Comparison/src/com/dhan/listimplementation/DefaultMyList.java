2c4f1a9ac7c16fc725ca61e6d7c7007963786163
Dhanush <danush2228s@gmail.com>
2023-11-16 21:15:32 +0530

    Practiced new topics colections, list with some hands on problems

---
+++ List Comparison/src/com/dhan/listimplementation/DefaultMyList.java
@@ -0,0 +1,55 @@
+package com.dhan.listimplementation;
+
+//import org.w3c.dom.Node;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DefaultMyList implements MyList{
+	
+	
+	
+	
+
+	@Override
+	public void add(Object e) {
+		
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(MyList c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
